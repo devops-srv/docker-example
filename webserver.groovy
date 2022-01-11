58e9d01c84413bb1ef38f10a909e924716d9929f
devops-srv <68237630+devops-srv@users.noreply.github.com>
2022-01-11 07:41:27 +0530

    Update webserver.groovy

--- webserver.groovy
+++ webserver.groovy
@@ -4,7 +4,7 @@ pipeline {
        stages {
             stage('SCM checkout') {
                   steps {
-                        git url: 'https://github.com/devops-srv/pipelinetestbatch17.git'
+                        git branch:'main', url: 'https://github.com/devops-srv/pipelineB29_project.git'
                         }
              }
              
@@ -14,7 +14,7 @@ pipeline {
                         }
               }
               
-              stage('transfer artifacts') {
+              stage('Deployment') {
                     steps {
                           sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(excludes: '', execCommand: '', execTimeout: 120000, flatten: true, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: true)])
                           }
