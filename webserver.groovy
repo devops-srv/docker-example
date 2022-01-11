92454dec725572801c4c490b4193479d06663d78
devops-srv <68237630+devops-srv@users.noreply.github.com>
2022-01-11 07:28:14 +0530

    Update webserver.groovy

--- webserver.groovy
+++ webserver.groovy
@@ -1,19 +1,23 @@
 pipeline {
     agent any
-    stages{
-        stage('scm checkout'){
-            steps{
-                git url: 'https://github.com/devops-srv/pipelineB29_project.git'
-            }
-        }
-        stage('Artifacts'){
-            steps{
-                archiveArtifacts '**/*.html'
-            }
-        }
-        stage('Deployment'){
-            steps{
-                sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfers(excludes: '', execCommand: '', execTimeout: 120000, flatten: true, makeEmptyDir: false, noDefaultExcludes: false, patternSeparator: '[,]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix:'',sourceFiles: '**/*.html')] usePromotionTimestamp: false, useworkspaceInpromotion: false, verbose: true)])
-            }
-        }
-    }
+               
+       stages {
+            stage('SCM checkout') {
+                  steps {
+                        git url: 'https://github.com/devops-srv/pipelinetestbatch17.git'
+                        }
+             }
+             
+             stage('archiving artifacts') {
+                  steps {
+                          archiveArtifacts '**/*.html'
+                        }
+              }
+              
+              stage('transfer artifacts') {
+                    steps {
+                          sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(excludes: '', execCommand: '', execTimeout: 120000, flatten: true, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: true)])
+                          }
+              }
+       }
+}
